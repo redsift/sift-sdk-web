06b75199bb42dee0dd5f8c948290b05269239bf5
Christos Vontas <christos@redsift.io>
2016-08-03 17:53:01 +0100

    Can't iterate over methods, need to export all of them

--- src/lib/sift-storage.js
+++ src/lib/sift-storage.js
@@ -3,11 +3,20 @@ import Observable from '@redsift/observable';
 export default class SiftStorage extends Observable {
   constructor() {
     super();
+    this._treo = null;
   }
 
   init(treo) {
-    Object.keys(treo).forEach((method) => {
-      this[method] = treo[method];
-    });
+    this._treo = treo;
   }
+
+  get(d){ return this._treo.get(d)}
+  getIndexKeys(d){ return this._treo.getIndexKeys(d)}
+  getIndex(d){ return this._treo.getIndex(d)}
+  getWithIndex(d){ return this._treo.getWithIndex(d)}
+  getAllKeys(d){ return this._treo.getAllKeys(d)}
+  getAll(d){ return this._treo.getAll(d)}
+  getUser(d){ return this._treo.getUser(d)}
+  putUser(d){ return this._treo.putUser(d)}
+  delUser(d){ return this._treo.delUser(d)}
 }
