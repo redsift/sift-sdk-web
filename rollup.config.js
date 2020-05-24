b226fc9f27f58fc9237b073a424b80a4b373e782
jordanw7 <38002117+JordanW7@users.noreply.github.com>
2020-05-24 17:21:09 +0100

    babel config

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,16 @@ import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 const babelConfig = {
-  presets: [['@babel/preset-env', { modules: 'umd' }]],
+  presets: [['@babel/preset-env']],
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
   ],
+};
+
+const babelConfigUmd = {
+  presets: [['@babel/preset-env', { modules: 'umd' }]],
+  plugins: babelConfig.plugins,
 };
 
 export default [
@@ -20,12 +25,13 @@ export default [
         file: pkg.module,
         format: 'es',
         name: 'sift-sdk-web',
+        plugins: [getBabelOutputPlugin(babelConfig)],
       },
       {
         file: pkg.browser,
         format: 'esm',
         name: 'sift-sdk-web',
-        plugins: [getBabelOutputPlugin(babelConfig)],
+        plugins: [getBabelOutputPlugin(babelConfigUmd)],
       },
     ],
   },
@@ -37,12 +43,13 @@ export default [
         file: 'dist/react.esm.js',
         format: 'es',
         name: 'sift-sdk-web/react',
+        plugins: [getBabelOutputPlugin(babelConfig)],
       },
       {
         file: 'dist/react.umd.js',
         format: 'esm',
         name: 'sift-sdk-web/react',
-        plugins: [getBabelOutputPlugin(babelConfig)],
+        plugins: [getBabelOutputPlugin(babelConfigUmd)],
       },
     ],
   },
